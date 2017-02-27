facc41fe54249e802745ee86c32450d619078035
Thomas Teh <gimaik@gmail.com>
2017-02-27 23:11:06 +0000

    updated vertigo

--- 495 CW2/HMMViterbiContinuous.m
+++ 495 CW2/HMMViterbiContinuous.m
@@ -4,6 +4,7 @@ function [decode] = HMMViterbiContinuous(Y,N,T,pi, A, mu, sigma2)
     decode = zeros(size(Y));
     delta =  zeros(NbLatent, N, T);
     delta_i = zeros(NbLatent, N);
+    backtrack =  zeros(NbLatent, N, T);
     z       = zeros(size(A,1),N,T);
     
     % Generating the emission probability for the sequence
@@ -18,18 +19,22 @@ function [decode] = HMMViterbiContinuous(Y,N,T,pi, A, mu, sigma2)
     
     % Filling up the delta table for time t=2:T
     for k = 1: NbLatent
-       for t=2: 2
+       for t=2: T
            for n= 1: N
-               delta_i(k,n) = max(A(:,k)+ delta(:,n,t-1));    
+               [delta_i(k,n), backtrack(k,n,t)] = max(A(:,k)+ delta(:,n,t-1));    
            end
            delta(:,:,t) =  delta_i+z(:,:,t);
        end
     end
     
-    % Return the decoding
-    for t=1:T
-        [M, I] = max(delta(:,:,t));
-        decode(:,t) = I.';
+    [M,decode(:,T)] = max(delta(:,:,T));
+   
+    % Decoding the delta array.
+    for t=T-1:-1:1
+        for n=1:N
+            decode(n,t) = backtrack(decode(n,t+1),n,t+1);
+        end
     end
+    
     
 end
--- 495 CW2/continuous_obs.m
+++ 495 CW2/continuous_obs.m
@@ -5,7 +5,7 @@ clc;
 % DATA GENERATION %
 %%%%%%%%%%%%%%%%%%%
 N  = 100;         % number of sequences
-T  = 500;        % length of the sequence
+T  = 100;        % length of the sequence
 
 % Initial Probability at time 1
 pi = [0.7; 0.3]; 
@@ -26,12 +26,12 @@ E.sigma2=[0.4 0.8]; %%the variances
 % EM ALGORITHM %
 %%%%%%%%%%%%%%%%
 % Initializing parameters
-pi = [0.51; 0.49]; 
-A  = [0.40 0.60 ; 0.40 0.60 ];        
+pi = [0.7; 0.3];  
+A  = [0.30 0.70 ; 0.40 0.60 ];        
 
 % One Dimensional Gaussians 
-E.mu    =[1 5.0]; %%the means of each of the Gaussians
-E.sigma2=[3 50]; %%the variances
+E.mu    =[0.1 5.0]; %%the means of each of the Gaussians
+E.sigma2=[0.4 0.8]; %%the variances
 
 
 % Running EM Algorithm
